4b901c0198ac7efc393c397efb5048f7a84f2bd2
VioletaMontesino <viomont76@gmail.com>
2021-04-08 20:35:30 +0200

    Subiendo los archivos

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+
+var elVideo = document.getElementById("video1");
+let btnPlay = document.getElementById("play");
+
+btnPlay.innerHTML = "Reproducir \u23EF"
+
+function Reproducir(){
+ 
+    
+    if (elVideo.paused) {
+        elVideo.play();
+       
+        btnPlay.innerHTML = "Pausar \u23F8";
+    }   
+    else {
+        elVideo.pause();
+        btnPlay.innerHTML = "Reproducir \u23EF";
+    }
+}
+
+function Stop(){
+    elVideo.pause();
+    elVideo.currentTime = 0;
+}       
+
+function BajarVolumen(){
+    console.log(elVideo.volume)
+    if(elVideo.volume > 0.1) {
+
+        elVideo.volume -= 0.1;
+    }
+}
+
+function SubirVolumen(){
+    console.log(elVideo.volume)
+    if(elVideo.volume < 1) {
+        elVideo.volume += 0.1;
+    }
+}
